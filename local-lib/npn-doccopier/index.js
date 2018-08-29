b4bf8593f957cdd747091c4ed8e6314f234d3fb3
npnm <nupindev@gmail.com>
2018-08-29 23:43:42 +0530

    updated docs

--- local-lib/npn-doccopier/index.js
+++ local-lib/npn-doccopier/index.js
@@ -3,59 +3,66 @@
 const fs = require('fs-extra');
 const jsdom = require('jsdom');
 const {
-    JSDOM
+  JSDOM
 } = jsdom;
 const path = require('path');
 
 fs.readJson(__dirname + '/build-config.json', (err, config) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    var source = "";
-    var destination = "";
-    if (config.copyDir) {
-        if (config.basePath && config.basePath != "") {
-            source = path.join(__dirname, config.basePath, config.copyDir.source);
-            destination = path.join(__dirname, config.basePath, config.copyDir.destination);
-        } else {
-            source = config.copyDir.source;
-            destination = config.copyDir.destination;
-        }
-    }
-    if (source != "" && destination != "") {
-        //copy directory
-        fs.copy(source, destination, err => {
-            if (err) {
-                console.error(err);
-                return;
-            }
-            console.log(`Files are copied successfully from ${config.copyDir.source} to ${config.copyDir.destination}`);
-            if (config.htmlBaseUrl) {
-                updateIndexPage(destination, config.htmlBaseUrl);
-            }
-        });
+  if (err) {
+    console.error(err);
+    return;
+  }
+  var source = "";
+  var destination = "";
+  if (config.copyDir) {
+    if (config.basePath && config.basePath != "") {
+      source = path.join(__dirname, config.basePath, config.copyDir.source);
+      destination = path.join(__dirname, config.basePath, config.copyDir.destination);
     } else {
-        console.error("no valid source and destination!");
+      source = config.copyDir.source;
+      destination = config.copyDir.destination;
     }
+  }
+  if (source != "" && destination != "") {
+    //clearing directory
+    fs.emptyDir(destination, err => {
+      if (err) {
+        return console.error(err);
+      }
+      //copy directory
+      fs.copy(source, destination, err => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log(`Files are copied successfully from ${config.copyDir.source} to ${config.copyDir.destination}`);
+        if (config.htmlBaseUrl) {
+          updateIndexPage(destination, config.htmlBaseUrl);
+        }
+      });
+    });
+
+  } else {
+    console.error("no valid source and destination!");
+  }
 });
 
 
 function updateIndexPage(fileocation, htmlBaseUrl) {
-    var filePath = path.join(fileocation, "index.html");
-    fs.exists(filePath, exists => {
-        if (exists) {
-            JSDOM.fromFile(filePath, {}).then(dom => {
-                dom.window.document.querySelector("base").setAttribute("href", htmlBaseUrl);
-                var finalHtml = dom.serialize();
-                fs.writeFile(filePath, finalHtml, err => {
-                    if (err) {
-                        console.err(err);
-                        return;
-                    }
-                    console.log("Updated index page successfully");
-                })
-            })
-        }
-    })
+  var filePath = path.join(fileocation, "index.html");
+  fs.exists(filePath, exists => {
+    if (exists) {
+      JSDOM.fromFile(filePath, {}).then(dom => {
+        dom.window.document.querySelector("base").setAttribute("href", htmlBaseUrl);
+        var finalHtml = dom.serialize();
+        fs.writeFile(filePath, finalHtml, err => {
+          if (err) {
+            console.err(err);
+            return;
+          }
+          console.log("Updated index page successfully");
+        })
+      })
+    }
+  })
 }
